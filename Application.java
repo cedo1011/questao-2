c3970f82ea738bba8d8c71689d44e6e975ed652b
cedo1011 <carloseduardodeoliveira11@gmail.com>
2022-05-16 16:58:27 +0000

    teste da prova

---
+++ Application.java
@@ -0,0 +1,20 @@
+package console;
+
+public class Application {
+    public static void main(String[] args) {
+        Figure figures[] = new Figure[3];
+        figures[0] = new Square();
+        figures[0].setName("Quadrado 1");
+        figures[1] = new Triangle();
+        figures[1].setName("Triângulo 1");
+        figures[2] = new Triangle();
+        figures[2].setName("Triângulo 2");
+        for(Figure fig : figures) {
+            fig.draw();
+        }
+        System.out.println("Abacaxi");
+        for(Figure fig : figures) {
+            fig.drawWithName();
+        }
+    }
+}
